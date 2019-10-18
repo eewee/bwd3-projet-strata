bc5aa2333278ea18ddda0a53b573ade763d59dbe
Michael <contact@eewee.fr>
2019-10-18 18:55:25 +0200

    Setup projet and first use variable.

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// Variables pour les réseaux sociaux :
+$twitter = "https://twitter.com/ExceliaGroupLR";
+$github = "https://github.com/eewee/";
+$dribbble = "https://dribbble.com/";
+
+// Variables pour le contact :
+$email = "contact@tld.com";
+$tel = "01 02 03 04 05";
+
+// Variables pour l'adresse :
+$adresse = "55 Av. Albert Einstein";
+$cp = 17000;
+$ville = "La Rochelle";
+$pays = "France";
